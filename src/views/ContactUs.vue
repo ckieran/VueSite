9cda737cb34fd085af1c7d8a6747ece2a9151a99
ckieran <ckieran@gmail.com>
2018-09-18 23:10:55 +0800

    added font awesome and a personal details component skeleton

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="contact">
     <h1>Contact Us</h1>
-    <p>our contact details include:</p>
+    <p>our contact details include: </p>
+    <div class="aligned">
     <ul>
-      <li>tel: <a href="tel:0892200800">0892200800</a></li>
-      <li><a href="mailto:contactus@mydomain.com">contactus@mydomain.com</a></li>
+      <li><font-awesome-icon icon="phone" /><a href="tel:0892200800">0892200800</a></li>
+      <li><font-awesome-icon icon="envelope" /> <a href="mailto:contactus@mydomain.com">contactus@mydomain.com</a></li>
     </ul>
+    </div>
   </div>
 </template>
 
@@ -13,8 +15,16 @@
 ul {
   list-style-type: none;
   padding: 0;
+  text-align: left;
 }
 a {
   color: #42b983;
+}
+div.aligned {
+  width:250px;
+  margin:auto;
+}
+.aligned a {
+  margin-left:10px;
 }
 </style>
