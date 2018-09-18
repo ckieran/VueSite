c394053294b08cae5568abbf77ba372a29f55e10
ckieran <ckieran@gmail.com>
2018-09-18 16:55:16 +0800

    added a contact us page

---
+++ src/views/ContactUs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="contact">
+    <h1>Contact Us</h1>
+    <p>our contact details include:</p>
+    <ul>
+      <li>tel: <a href="tel:0892200800">0892200800</a></li>
+      <li><a href="mailto:contactus@mydomain.com">contactus@mydomain.com</a></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
+}
+</style>
